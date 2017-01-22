59b4ee9e0cfb9d02c0249cf71425f2bc97e9a29c
Tyler Parker <computemachines@gmail.com>
2017-01-22 14:12:23 -0500

    Clean static dir and git pull on deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+git pull
+
+rm -rfv /var/www/computemachines/
 mkdir -pv /var/www/computemachines/
 cp -rv /root/blog/computemachines/static/ /var/www/computemachines/
 cp -v /root/blog/site-computemachines.com.conf /etc/nginx/sites-available/
+
+nginx -s reload
