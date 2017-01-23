3debbde3d8abe920dd032fab99ed283963f1a7f7
Tyler Parker <computemachines@gmail.com>
2017-01-23 02:23:17 -0500

    Posts are stored as reST markup with math formatting done by MathJax.

--- computemachines/app.py
+++ computemachines/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-    render_template, flash
-from werkzeug.contrib.fixers import LighttpdCGIRootFix
+    render_template, flash, Markup
+from flask_pymongo import PyMongo
+from docutils.core import publish_parts
 
 app = Flask(__name__)
-app.wsgi_app = LighttpdCGIRootFix(app.wsgi_app)
+mongo = PyMongo(app)
+
 app.config.from_object(__name__)
 
 app.config.update(dict(
@@ -13,11 +15,19 @@ app.config.from_envvar('APP_SETTINGS', silent=True)
 
 @app.route('/')
 def show_last_post():
-    return render_template('show_post.html', post_id=-1)
+    return render_template('show_post.html', post_id=0)
+
+@app.route('/post/new/<int:post_id>')
+def new_post(post_id):
+    mongo.db.posts.insert({'_id': post_id, 'content': 'hello worlds' })
+    return redirect(url_for('show_post', post_id=post_id))
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
-    return render_template('show_post.html', post_id=post_id)
+    post = mongo.db.posts.find_one_or_404({'_id': post_id})
+    post_content_html = publish_parts(post['content'], writer_name='html',
+                                      settings_overrides={"math_output":"MathJax"})['html_body']
+    return render_template('show_post.html', post_content_html=Markup(post_content_html))
 
 @app.route('/projects/')
 def list_projects():
@@ -29,3 +39,6 @@ def login():
         pass
     else:
         return render_template('login.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,4 @@ cp -rv /root/blog/computemachines/static/ /var/www/computemachines/
 cp -v /root/blog/site-computemachines.com.conf /etc/nginx/sites-available/
 
 nginx -s reload
+touch /tmp/reload_uwsgi
