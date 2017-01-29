f1c7dcd8d5b9d8f9b38796db1f68663b421e297e
Tyler Parker <computemachines@gmail.com>
2017-01-28 20:11:01 -0500

    Script to populate db.

---
+++ db/fill.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+from pymongo import MongoClient
+from pathlib import Path
+
+client = MongoClient()
+db = client.app
+
+db.posts.drop()
+
+for f in Path('posts').iterdir():
+    if f.is_dir():
+        continue
+    _id = int(f.stem)
+    content = f.open().read()
+    db.posts.insert_one({
+        "_id": _id,
+        "format": f.suffix,
+        "content": content
+    })
+    print(db.posts.find_one({"_id": _id}))
