b9cf2d168894b3674db2346c92e8b858a79c61da
Tyler Parker <computemachines@gmail.com>
2017-02-06 22:45:39 -0500

    Working test of new glutils module.

--- computemachines/static/program.js
+++ computemachines/static/program.js
@@ -17,45 +17,6 @@ $(function() {
         if (!gl.getExtension("OES_standard_derivatives")) {
             throw new Error("does not support OES_standard_derivatives");
         }
-        var attributes = {};
-        var uniforms = {};
-
-        var createShader = function(source, type) {
-            //type = gl.VERTEX_SHADER;
-            //type = gl.FRAGMENT_SHADER;
-            var shader = gl.createShader(type);
-            gl.shaderSource(shader, source);
-            gl.compileShader(shader);
-            var compileStatus = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-            if (!compileStatus) {
-                console.log("failed to compile: "+source);
-                console.log(gl.getShaderInfoLog(shader));
-                gl.deleteShader(shader);
-                return;
-            }
-            return shader;
-        };
-
-        var program = function() {
-            var vertShader = createShader(
-                $(".vert-shader.common-shader").text(), gl.VERTEX_SHADER);
-            var fragShaderCommonSource = $(".frag-shader.common-shader").text();
-            var fragShaderLocalSource = canvas.next().children(".frag-shader").text();
-            var fragShader = createShader(
-                fragShaderCommonSource + fragShaderLocalSource, gl.FRAGMENT_SHADER);
-            var program = gl.createProgram();
-            glerror();
-            gl.attachShader(program, vertShader);
-            glerror();
-            gl.attachShader(program, fragShader);
-            glerror();
-            gl.linkProgram(program);
-            glerror();
-            var linkStatus = gl.getProgramParameter(program, gl.LINK_STATUS);
-            if (!linkStatus) {
-                console.log("program failed to link");
-                return;
-            }
             attributes.combined = gl.getAttribLocation(program, "a_vec4_combined");
             uniforms.mouse = gl.getUniformLocation(program, "u_vec2_mouse");
             return program;
@@ -64,16 +25,9 @@ $(function() {
         var buffer = function() {
             var buffer = gl.createBuffer();
             gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-            var verts = [
-                1., 1., 1., 1.,
-                -1., 1., 0., 1.,
-                1., -1., 1., 0.,
-                -1, -1, 0, 0
-            ];
-            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
             return buffer;
         }();
-        gl.clearColor(0., 0., 0., 1.);
 
         var draw = function() {
             glerror();
@@ -85,7 +39,6 @@ $(function() {
             glerror();
             console.log("mouse: ", mouse);
             gl.uniform2f(uniforms.mouse, mouse.x/canvas[0].offsetWidth, 1-mouse.y/canvas[0].offsetHeight);
-            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
         };
 
     });
--- computemachines/static/scripts/app/main.js
+++ computemachines/static/scripts/app/main.js
@@ -1,3 +1,20 @@
 define(["jquery", "glutils"], function ($, GL) {
-    console.log(new GL($('.gl-ide canvas')[0]));
+    var gl = new GL($('.gl-ide canvas')[0], {
+        vertSource:
+        "attribute vec4 a_vec4_combined;\n" +
+            "varying vec2 v_vec2_texCoord;\n" +
+            "void main() {\n" +
+            "  v_vec2_texCoord = a_vec4_combined.zw;\n" +
+            "  gl_Position = vec4(a_vec4_combined.xy, 0, 1);\n" +
+            "}",
+        fragSource:
+        "precision mediump float;\n" +
+        "varying vec2 v_vec2_texCoord;\n" +
+            "void main() {\n" +
+            "  gl_FragColor = vec4(0, 1, 0, 1);\n;" +
+            "}",
+        attributes: {combined: "a_vec4_combined"}
+
+    });
+    gl.draw();
 });
