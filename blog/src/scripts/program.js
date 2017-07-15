7dec02e791c9ff161d33b4d14c7251bbed75268c
Tyler Parker <tparker@computemachines.com>
2017-07-15 17:24:33 -0400

    Test of more complex app in production env.

--- blog/src/scripts/program.js
+++ blog/src/scripts/program.js
@@ -1,7 +1,33 @@
 import $ from 'jquery';
-//var $ = require('jquery');
-import '../style/style.css';
+const Snap = require(`imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js`);
 
-$('<h1/>', {
-  text: 'Hello JQuery'
-}).appendTo($('body'));
+
+console.log("program run");
+
+// import '../style/style.css';
+window.$ = $; // TODO: for development only
+
+
+let circle = Snap("svg circle");
+$("svg")
+  .on("mouseover", "circle", function() {
+    circle.animate({r: 100}, 400);
+  })
+  .on("mouseout", "circle", function() {
+    circle.animate({r: 20}, 400);
+  });
+
+
+
+
+// import 'imports-loader?this=>window!mathjax/MathJax.js';
+
+let message = "Hello worlds from JQuery";
+
+$.map([1, 2, 3],
+      (e)=>(
+	$(`<h${e} />`,{
+	  text: `Header: ${Math.sqrt(e+1)}.`
+	}).appendTo($('body'))
+      ));
+[1, 2,"67",  3, 4].map(x=>console.log(x));
--- blog/webpack.config.js
+++ blog/webpack.config.js
@@ -1,19 +1,32 @@
-var path = require('path');
+const {resolve} = require('path');
 
-module.exports = {
-  entry: './src/scripts/program.js',
-  output: {
-    path: path.resolve(__dirname, './computemachines/static/'),
-    publicPath: "/static/",
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }, {
-      test: /\.(png|svg|jpg|gif)$/,
-      use: ['file-loader'] //?name=/static/[hash].[ext]']
-    }]
-  }
+module.exports = () => {
+  return {
+    context: resolve('src'),
+    entry: './scripts/program.js',
+    output: {
+      path: resolve(__dirname, './computemachines/static/'),
+      publicPath: "/static/",
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+	test: /\.js$/,
+	use: {
+	  loader: 'babel-loader',
+	  options: {
+	    presets: ['es2015']
+	  }
+	},
+	exclude: /node_modules/
+      }, {
+	test: /\.css$/,
+	use: ['style-loader', 'css-loader']
+      }, {
+	test: /\.(png|svg|jpg|gif)$/,
+	use: ['file-loader'] //?name=/static/[hash].[ext]']
+      }]
+    },
+    devtool: "eval" // TODO: necessary for production?
+  };
 };
