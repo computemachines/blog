ade25e1592d6d69dbdaed2c086004ec07fed2b9d
Tyler Parker <tparker@computemachines.com>
2017-07-16 23:17:04 -0400

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,25 @@
 containers="uwsgi nginx webpack"
 
 # build images
-for $container in $containers; do
+for container in $containers; do
     pushd $container
     docker build -t $container . && echo "success"
     popd
 done
 
-nginx_development="echo docker run --rm -tid -p 80:80 -p 443:443 --net=blog --name=nginx -v /assets/ -v /etc/self-cert/live/:/etc/letsencrypt/live/ -v /etc/self-cert/:/etc/self-cert/ -v /etc/ssl/:/etc/ssl nginx nginx"
+docker_flags="--rm -ti"
+daemonize="-d"
 
-nginx_production="echo docker run -tid -p 80:80 -p 443:443 -v /etc/letsencrypt/:/etc/letsencrypt -v /assets/ -v /etc/ssl/:/etc/ssl/ --net=blog --name=nginx nginx nginx"
+nginx_development="docker run $docker_flags $daemonize \
+-p 80:80 -p 443:443 --net=blog --name=nginx -v /assets/ \
+-v /etc/self-cert/live/:/etc/letsencrypt/live/ \
+-v /etc/self-cert/:/etc/self-cert/ \
+-v /etc/ssl/:/etc/ssl nginx"
+
+nginx_production="docker run $docker_flags $daemonize \
+-p 80:80 -p 443:443 \
+-v /etc/letsencrypt/:/etc/letsencrypt -v /assets/ \
+-v /etc/ssl/:/etc/ssl/ --net=blog --name=nginx nginx"
 
 uwsgi="docker run --rm -tid --net=blog --name=uwsgi uwsgi"
 
