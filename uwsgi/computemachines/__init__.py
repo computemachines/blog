8c8171bdc8c9ec771f83a6c91b44d8f9fc1c4d90
Tyler Parker <tparker@computemachines.com>
2017-08-09 20:02:12 -0400

    Testing site-wide critical.css.

--- uwsgi/computemachines/__init__.py
+++ uwsgi/computemachines/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
+def get_resource_as_string(name, charset='utf-8'):
+    with app.open_resource(name) as f:
+        return f.read().decode(charset)
+
+app.jinja_env.globals['get_resource_as_string'] = get_resource_as_string
+
+
+@app.context_processor
+def inject_debug():
+    return dict(debug=app.debug)
+
+
 @app.route('/')
 def index():
     return redirect(url_for("show_projects"))
@@ -8,7 +20,3 @@ def index():
 @app.route('/projects')
 def show_projects():
     return render_template("projects/elasticity.html")
-
-@app.context_processor
-def inject_debug():
-    return dict(debug=app.debug)
